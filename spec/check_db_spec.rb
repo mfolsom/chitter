494cb8dc92f27a154d7a6717c568bd4aaa4f796a
Megan Folsom <mfolsom@gmail.com>
2014-02-14 12:28:08 +0000

    altered table to accept longer text

--- spec/check_db_spec.rb
+++ spec/check_db_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe Post do
   it 'should be created then retrieved from the db' do
     expect(Post.count).to eq(0)
-    Post.create(:body =>"What's up peeps?")
+    Post.create(:body =>"What's up peeps? Another fine test we've gotten ourselves into!")
     # expect(Peep.count).to eq(1)
     message = Post.first
     puts message
-    expect(message.body).to eq("What's up peeps?")
+    expect(message.body).to eq("What's up peeps? Another fine test we've gotten ourselves into!")
     message.destroy
     expect(Post.count).to eq(0) 
   end
